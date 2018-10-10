1550aa1cb40c2b4ea1f39ff46055caa6754d4cfd
SIANSS <acsian7@gmail.com>
2018-10-10 15:32:15 +0530

    data

---
+++ public/script.js
@@ -0,0 +1,25 @@
+function showPlayer() {
+      document.getElementById('player').style.display = 'inline';
+      document.getElementById('hider').style.display = 'none';
+      showGame();
+      this.jumpTo(0)
+
+
+
+  }
+  function showGame() {
+    document.getElementById('root').style.display = 'inline';
+  formMsg();
+
+
+  }
+  function formMsg() {
+    document.getElementById('formmsg').style.display = 'none';
+
+
+  }
+  function startMsg() {
+
+  }
+
+console.log(window.location);
